fff72a8ec4154608c2f4e253333df15dee2864f6
robin <comfysagedev@gmail.com>
2025-11-23 12:32:46 +0100

    fix(plugin): clean up highlights

--- plugin/artio.lua
+++ plugin/artio.lua
@@ -9,29 +9,23 @@ local augroup = vim.api.nvim_create_augroup("artio:hl", {})
 vim.api.nvim_create_autocmd("ColorScheme", {
   group = augroup,
   callback = function()
+    local hi = function(name, opts)
+      opts.default = true
+      vim.api.nvim_set_hl(0, name, opts)
+    end
+
     local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
     local msgarea_hl = vim.api.nvim_get_hl(0, { name = "MsgArea" })
 
-    vim.api.nvim_set_hl(0, "ArtioNormal", { fg = normal_hl.fg, bg = msgarea_hl.bg, default = true })
-    vim.api.nvim_set_hl(0, "ArtioPrompt", { link = "Title", default = true })
+    hi("ArtioNormal", { fg = normal_hl.fg, bg = msgarea_hl.bg })
+    hi("ArtioPrompt", { link = "Title" })
 
     local cursor_hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
     local cursorline_hl = vim.api.nvim_get_hl(0, { name = "CursorLine" })
-    vim.api.nvim_set_hl(0, "ArtioSel", { fg = cursor_hl.bg, bg = cursorline_hl.bg, default = true })
-    vim.api.nvim_set_hl(0, "ArtioPointer", { fg = cursor_hl.bg, default = true })
+    hi("ArtioSel", { fg = cursor_hl.bg, bg = cursorline_hl.bg })
+    hi("ArtioPointer", { fg = cursor_hl.bg })
 
-    vim.api.nvim_set_hl(0, "ArtioMatch", { link = "PmenuMatch", default = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd("ColorSchemePre", {
-  group = augroup,
-  callback = function()
-    vim.api.nvim_set_hl(0, "ArtioNormal", {})
-    vim.api.nvim_set_hl(0, "ArtioPrompt", {})
-    vim.api.nvim_set_hl(0, "ArtioSel", {})
-    vim.api.nvim_set_hl(0, "ArtioPointer", {})
-    vim.api.nvim_set_hl(0, "ArtioMatch", {})
+    hi("ArtioMatch", { link = "PmenuMatch" })
   end,
 })
 
